2c2268c8bdfcb57160893779e66ea5968759be65
Anqi Pang <anqipans@amazon.com>
2022-08-04 13:34:47 -0700

    initial commit

---
+++ Formula/demo.rb
@@ -0,0 +1,13 @@
+require_relative "lib/private"
+class DemoPrivate < Formula
+  desc "Kevin's homebrew tap to test ruby dependency and version management"
+  homepage ""
+  url "https://github.com/AnqiPang/demo-repo/archive/refs/tags/0.1.0.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  sha256 "51704a66a5bac02de7a6c325be04f29e5413b197b666de22865cc2d3ced111d9"
+  license "Apache-2.0"
+
+  def install
+    bin.install "src/HelloWorld.go"
+    puts "Demo tap installed"
+  end
+end
