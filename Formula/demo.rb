45ebf7e47b4b92834b8ad037d0d910700376dda3
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-08-14 23:42:02 -0700

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -1,11 +1,10 @@
-require_relative "../lib/private.rb"
+require_relative "../lib/private"
 class Demo < Formula
   desc "Anqi's homebrew tap"
-  homepage ""
-  url "https://github.com/AnqiPang/demo-repo.git", 
+  homepage "https://github.com/AnqiPang/demo-repo.git"
+  url "https://github.com/AnqiPang/demo-repo.git",:using => GitHubPrivateRepositoryDownloadStrategy,
       tag: "demo-0.2.0",
       revision: "12c659549faf6da67451ccbec1dcbc25bcb65ba7",
-      :using => GitHubPrivateRepositoryDownloadStrategy
   license "Apache-2.0"
 
   def install
