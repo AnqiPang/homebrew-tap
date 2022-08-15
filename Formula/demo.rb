58fc5434f11d0beb10e6ace6418ab53eef027d31
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-08-14 23:52:37 -0700

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -3,7 +3,7 @@ class Demo < Formula
   desc "Anqi's homebrew tap"
   homepage "https://github.com/AnqiPang/demo-repo"
   url "https://github.com/AnqiPang/demo-repo.git", 
-      tag: "demo-0.2.0",
+      tag:      "demo-0.2.0",
       revision: "12c659549faf6da67451ccbec1dcbc25bcb65ba7"
   license "Apache-2.0"
 
