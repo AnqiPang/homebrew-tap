13d487cc9973dbfe5c23adf2adb6030c872d7006
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-08-14 23:29:29 -0700

    use tag and revision with url

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -2,7 +2,10 @@ require_relative "../lib/private.rb"
 class Demo < Formula
   desc "Anqi's homebrew tap"
   homepage ""
-  url "https://github.com/AnqiPang/demo-repo/archive/refs/tags/0.1.0.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/AnqiPang/demo-repo", 
+      tag: "demo-0.2.0",
+      revision: "12c659549faf6da67451ccbec1dcbc25bcb65ba7",
+      :using => GitHubPrivateRepositoryDownloadStrategy
   sha256 "51704a66a5bac02de7a6c325be04f29e5413b197b666de22865cc2d3ced111d9"
   license "Apache-2.0"
   version "0.1.0"
