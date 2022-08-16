27eb53992c362b8b6fe2cfdb4929f7a20007e503
re0bot <arisuyukine@gmail.com>
2022-08-16 22:54:07 +0000

    demo 0.3.6

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -3,8 +3,8 @@ class Demo < Formula
   desc "Anqi's homebrew tap"
   homepage "https://github.com/AnqiPang/demo-repo"
   url "https://github.com/AnqiPang/demo-repo.git",
-      tag:      "demo-0.3.0",
-      revision: "aec678ec192694e2e060f254101ee20fb6b2316c"
+      tag:      "demo-0.3.6",
+      revision: "6940eb5b5f9961ee94b79c0e983fbf50cfbf2a94"
   license "Apache-2.0"
 
   def install
