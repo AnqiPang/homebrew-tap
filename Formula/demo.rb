c85f81c2a9031320447de1f11be3fad525eca839
re0bot <arisuyukine@gmail.com>
2022-08-17 19:19:49 +0000

    demo 0.4.1

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -3,8 +3,8 @@ class Demo < Formula
   desc "Anqi's homebrew tap"
   homepage "https://github.com/AnqiPang/demo-repo"
   url "https://github.com/AnqiPang/demo-repo.git",
-      tag:      "demo-0.3.9",
-      revision: "6246feb41fb406300bca33ebfa6688d1bd129749"
+      tag:      "demo-0.4.1",
+      revision: "23b6b99026e7d102c87ea538c1afecc973d27dbb"
   license "Apache-2.0"
   depends_on "go" => :build
   def install
