ffeb9f4246c8432780bb40a8bda18c53aba04c04
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-08-17 11:01:27 -0700

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -6,9 +6,7 @@ class Demo < Formula
       tag:      "demo-0.3.9",
       revision: "6246feb41fb406300bca33ebfa6688d1bd129749"
   license "Apache-2.0"
-  
   depends_on "go" => :build
-  
   def install
     system "go", "build", "greeter/main.go"
     bin.install "main"
